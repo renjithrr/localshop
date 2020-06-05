8a6d526a386282560bb1687a98696e5682732762
renjith <renjithr@qburst.com>
2020-06-05 20:01:00 +0530

    User and shop APIs

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from user.views import *
+
+urlpatterns = [
+    # path('login/', LoginView.as_view()),
+    path('verify-number/', VerifyMobileNumberView.as_view()),
+    path('verify-otp/', VerifyMobileOtpView.as_view()),
+    path('account-details/', AccountDetailsView.as_view()),
+    path('shop-details/', ShopDetailsView.as_view()),
+    path('location-info/', LocationDataView.as_view()),
+    # path('devices/', DeviceTokenView.as_view()),
+]
