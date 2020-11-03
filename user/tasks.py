a675803c930549ce5d4b2a719e36b1f8a589199f
renjithr <renjithr@qburst.com>
2020-11-03 19:35:55 +0530

    order history api changes

--- user/tasks.py
+++ user/tasks.py
@@ -167,104 +167,6 @@ def delivery_system_call(**data):
         db_logger.exception(e)
 
 
-# def render_to_pdf(template_src, context_dict):
-
-# Python program to print a given number in
-# words. The program handles numbers
-# from 0 to 9999
-
-# A function that prints
-# given number in words
-# def convert_to_words(num):
-#     # Get number of digits
-#     # in given number
-#     l = len(num)
-#
-#     # Base cases
-#     if (l == 0):
-#         print("empty string")
-#         return;
-#
-#     if (l > 4):
-#         print("Length more than 4 is not supported")
-#         return
-#
-#     # The first string is not used,
-#     # it is to make array indexing simple
-#     single_digits = ["zero", "one", "two", "three",
-#                      "four", "five", "six", "seven",
-#                      "eight", "nine"]
-#
-#     # The first string is not used,
-#     # it is to make array indexing simple
-#     two_digits = ["", "ten", "eleven", "twelve",
-#                   "thirteen", "fourteen", "fifteen",
-#                   "sixteen", "seventeen", "eighteen",
-#                   "nineteen"]
-#
-#     # The first two string are not used,
-#     # they are to make array indexing simple
-#     tens_multiple = ["", "", "twenty", "thirty", "forty",
-#                      "fifty", "sixty", "seventy", "eighty",
-#                      "ninety"]
-#
-#     tens_power = ["hundred", "thousand"]
-#
-#     # Used for debugging purpose only
-#     print(num, ":", end=" ")
-#
-#     # For single digit number
-#     if (l == 1):
-#         print(single_digits[ord(num[0]) - '0'])
-#         return
-#
-#     # Iterate while num is not '\0'
-#     x = 0
-#     while (x < len(num)):
-#
-#         # Code path for first 2 digits
-#         if (l >= 3):
-#             if (ord(num[x]) - 48 != 0):
-#                 print(single_digits[ord(num[x]) - 48],
-#                       end=" ")
-#                 print(tens_power[l - 3], end=" ")
-#             # here len can be 3 or 4
-#
-#             l -= 1
-#
-#         # Code path for last 2 digits
-#         else:
-#
-#             # Need to explicitly handle
-#             # 10-19. Sum of the two digits
-#             # is used as index of "two_digits"
-#             # array of strings
-#             if (ord(num[x]) - 48 == 1):
-#                 sum = (ord(num[x]) - 48 +
-#                        ord(num[x + 1]) - 48)
-#                 print(two_digits[sum])
-#                 return;
-#
-#             # Need to explicitely handle 20
-#             elif (ord(num[x]) - 48 == 2 and
-#                               ord(num[x + 1]) - 48 == 0):
-#                 print("twenty")
-#                 return;
-#
-#             # Rest of the two digit
-#             # numbers i.e., 21 to 99
-#             else:
-#                 i = ord(num[x]) - 48
-#                 if (i > 0):
-#                     print(tens_multiple[i], end=" ")
-#                 else:
-#                     print("", end="")
-#                 x += 1
-#                 if (ord(num[x]) - 48 != 0):
-#                     print(single_digits[ord(num[x]) - 48])
-#         x += 1
-
-
 @shared_task()
 def render_to_pdf(delivery_type, customer, order, delivery_charge):
     try:
@@ -327,62 +229,11 @@ def render_to_pdf(delivery_type, customer, order, delivery_charge):
                                     'total': value.total, 'cgst': round(cgst, 2),'sgst': round(sgst, 2),
                                     'igst': round(igst, 2), 'kfc': round(kfc, 2),
                                     'grand_total': grand_total})
-        if delivery_type == DELIVERY_CHOICES.pickup or delivery_type == DELIVERY_CHOICES.bulk_delivery:
+        if delivery_type == DELIVERY_CHOICES.pickup:
             product_details = product_details
-        elif delivery_type == DELIVERY_CHOICES.self_delivery:
-            x = delivery_charge
-            y = 100 * x / 129
-            kfc = y / 100
-            cgst = (0.2248062016 * x - kfc) / 2
-            sgst = (0.2248062016 * x - kfc) / 2
-            grand_total = x + cgst + sgst + kfc
-            product_details.append({'name': 'Delivery charge', 'quantity': 1, 'unit_prize': x,
-                                    'total': x, 'cgst': round(cgst, 2),'sgst': round(sgst, 2),
-                                    'igst': round(igst, 2), 'kfc': round(kfc, 2),
-                                    'grand_total': grand_total})
-            product_details = product_details
-            grand_sum += grand_total
-        elif delivery_type == DELIVERY_CHOICES.townie_ship:
-            # grand_total = value.total + cgst + sgst + kfc
+        elif delivery_type == DELIVERY_CHOICES.self_delivery or delivery_type == DELIVERY_CHOICES.bulk_delivery:
             delivery_details = order.shop.shop_delivery_options.last()
-            if float(grand_total) > delivery_details.free_delivery_for:
-                shipping_address = billing_address
-                sold_by = 'Townie'
-                townie_referal = AppConfigData.objects.get(key='TOWNIE_REFERRAL_PERCENTAGE').value
-                townie_referal = float(townie_referal) / 100
-                referal_fee = townie_referal * order.grand_total * 1.18
-                tsf = 0.0236 * order.grand_total
-                x = referal_fee + tsf
-                y = 100 * x / 129
-                kfc = y / 100
-                cgst = (0.2248062016 * x - kfc) / 2
-                sgst = (0.2248062016 * x - kfc) / 2
-                grand_total = x + cgst + sgst + kfc
-                townie_details = []
-                billing_address = vendor_address
-                townie_details.append({'name': 'Service charge', 'quantity': 1, 'unit_prize': x,
-                                        'total': x, 'cgst': round(cgst, 2), 'sgst': round(sgst, 2),
-                                        'igst': round(igst, 2), 'kfc': round(kfc, 2),
-                                        'grand_total': grand_total})
-                context = {
-                    "invoice": invoice_number,
-                    "billing_address": billing_address,
-                    "shipping_address": '',
-                    "order_no": order_id,
-                    "order_date": order.created_at,
-                    "invoice_date": created_at,
-                    "amount_words": "",
-                    "pan_no": pan_number,
-                    "gst_no": gst_number,
-                    "product_details": townie_details,
-                    # "vendor_address": vendor_address,
-                    "sold_by": sold_by
-                }
-                template = get_template('townie_Invoice.html')
-                html = template.render(context)
-                result = BytesIO()
-                pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
-            else:
+            if float(grand_total) < delivery_details.free_delivery_for:
                 x = delivery_charge
                 y = 100 * x / 129
                 kfc = y / 100
@@ -390,11 +241,74 @@ def render_to_pdf(delivery_type, customer, order, delivery_charge):
                 sgst = (0.2248062016 * x - kfc) / 2
                 grand_total = x + cgst + sgst + kfc
                 product_details.append({'name': 'Delivery charge', 'quantity': 1, 'unit_prize': x,
-                                        'total': x, 'cgst': round(cgst, 2), 'sgst': round(sgst, 2),
-                                        'igst': round(igst, 2), 'kfc': round(kfc,2 ),
+                                        'total': x, 'cgst': round(cgst, 2),'sgst': round(sgst, 2),
+                                        'igst': round(igst, 2), 'kfc': round(kfc, 2),
                                         'grand_total': grand_total})
                 product_details = product_details
                 grand_sum += grand_total
+            else:
+                product_details = product_details
+        elif delivery_type == DELIVERY_CHOICES.townie_ship:
+            product_details = product_details
+            shipping_address = billing_address
+            sold_by = 'Townie'
+            townie_referal = AppConfigData.objects.get(key='TOWNIE_REFERRAL_PERCENTAGE').value
+            townie_referal = float(townie_referal) / 100
+            referal_fee = townie_referal * order.grand_total * 1.18
+            tsf = 0.0236 * order.grand_total
+            x = referal_fee + tsf
+            y = 100 * x / 129
+            kfc = y / 100
+            cgst = (0.2248062016 * x - kfc) / 2
+            sgst = (0.2248062016 * x - kfc) / 2
+            grand_total = x + cgst + sgst + kfc
+            townie_details = []
+            billing_address = vendor_address
+            townie_details.append({'name': 'Service charge', 'quantity': 1, 'unit_prize': x,
+                                    'total': x, 'cgst': round(cgst, 2), 'sgst': round(sgst, 2),
+                                    'igst': round(igst, 2), 'kfc': round(kfc, 2),
+                                    'grand_total': grand_total})
+            context = {
+                "invoice": invoice_number,
+                "billing_address": billing_address,
+                "shipping_address": '',
+                "order_no": order_id,
+                "order_date": order.created_at,
+                "invoice_date": created_at,
+                "amount_words": "",
+                "pan_no": pan_number,
+                "gst_no": gst_number,
+                "product_details": townie_details,
+                # "vendor_address": vendor_address,
+                "sold_by": sold_by
+            }
+            template = get_template('townie_Invoice.html')
+            html = template.render(context)
+            file = open(settings.MEDIA_ROOT + str(invoice_number) + '.pdf', "w+b")
+            pisaStatus = pisa.CreatePDF(html.encode('utf-8'), dest=file,
+                                        encoding='utf-8')
+            file.seek(0)
+            pdf = file.read()
+            SENDER = 'townie.store@gmail.com'
+            deliver_email.apply_async(queue='normal', args=(pdf,
+                                                            SENDER,
+                                                            shop_email,
+                                                            str(invoice_number) + '.pdf'))
+            # deliver_email(pdf, customer_email, shop_email, str(invoice_number) + '.pdf')
+            file.close()
+
+            x = delivery_charge
+            y = 100 * x / 129
+            kfc = y / 100
+            cgst = (0.2248062016 * x - kfc) / 2
+            sgst = (0.2248062016 * x - kfc) / 2
+            grand_total = x + cgst + sgst + kfc
+            product_details.append({'name': 'Delivery charge', 'quantity': 1, 'unit_prize': x,
+                                    'total': x, 'cgst': round(cgst, 2), 'sgst': round(sgst, 2),
+                                    'igst': round(igst, 2), 'kfc': round(kfc,2 ),
+                                    'grand_total': grand_total})
+            product_details = product_details
+            grand_sum += grand_total
 
         context = {
             "invoice": invoice_number,
